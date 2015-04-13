7ba9d8d3940038117ccf43e5f3626ef966fafa94
othree <othree@gmail.com>
2015-04-13 18:30:24 +0800

    Map in node

--- src/quaternary.ts
+++ src/quaternary.ts
@@ -1,6 +1,6 @@
 
 class Quaternary {
-  children: Array<Quaternary|void>;
+  children: Quaternary[];
   data: any;
   constructor(base?: Quaternary) {
     var i;
@@ -15,6 +15,9 @@ class Quaternary {
     this.data = data;
   }
   setData(data: any): Quaternary {
+    if (data === this.data) {
+      return this;
+    }
     var nq = new Quaternary(this);
     nq._setData(data);
     return nq;
@@ -26,6 +29,9 @@ class Quaternary {
     this.children[i] = data;
   }
   setChild(i: number, data: Quaternary): Quaternary {
+    if (data === this.children[i]) {
+      return this;
+    }
     var nq = new Quaternary(this);
     nq._setChild(i, data);
     return nq;
@@ -35,5 +41,34 @@ class Quaternary {
   }
   hasChild(i: number): boolean {
     return this.children[i] ? true : false;
+  }
+  map(f: (any) => any): Quaternary {
+    var i, nq, flag = false;
+    var leaf = this.data, newleaf;
+    var child, newchild, newchildren = [];
+    if (leaf) {
+      newleaf = f(leaf);
+      if (newleaf && newleaf !== leaf) {
+        return this.setData(newleaf);
+      }
+    } else {
+      for (i in this.children) {
+        if (this.children[i]) {
+          child = this.children[i]
+          newchild = child.map(f);
+          if (child !== newchild) {
+            newchildren.push(i, newchild)
+          }
+        }
+      }
+      if (newchildren.length) {
+        nq = new Quaternary(this);
+        for (i = 0; i < newchildren.length; i++) {
+          nq._setChild(newchildren[i][0], newchildren[i][1]);
+        }
+        return nq;
+      }
+    }
+    return this;
   }
 };
